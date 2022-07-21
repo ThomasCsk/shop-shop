0a5b8a61c146a9a387c069d172e3910ef5fa98ac
Thomas Ciszek <tciszek515@gmail.com>
2022-07-21 14:23:25 -0400

    cart ui created

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_CATEGORIES } from '../../utils/queries';
 import { useStoreContext } from "../../utils/GlobalState";
-import React, { useEffect } from 'react';
+
 import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 
-function CategoryMenu({ setCategory }) {
+function CategoryMenu() {
   const [state, dispatch] = useStoreContext();
 
   const { categories } = state;
